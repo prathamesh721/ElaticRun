15b0ec50d7882fb74ade92ecc6d362cc250bbc51
prathamesh721 <prathamesh721@gmail.com>
2022-03-18 22:49:15 +0530

    Commit before fixing minor issues like font size

---
+++ app.js
@@ -0,0 +1,9 @@
+const menuBar = document.querySelector('.menuIcon');
+const nav = document.querySelector('.nav')
+
+
+
+
+           menuBar.addEventListener('click',()=>{
+               nav.classList.toggle('active')
+           })
