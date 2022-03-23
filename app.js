0f752d104e3c66da1d780e708c9c573038142bad
prathamesh721 <prathamesh721@gmail.com>
2022-03-23 15:24:09 +0530

    Commit before fixing searchbar issue

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+
+// collapsible Navbar
 const menuBar = document.querySelector('.menuIcon');
 const nav = document.querySelector('.nav')
-
-
-
 
            menuBar.addEventListener('click',()=>{
                nav.classList.toggle('active')
            })
+           
